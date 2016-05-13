5408995a7fb97e7af63baa0888ea8ee92689b195
Jo <jo@mt-asa1-1-vpn192.research.att.com>
2016-05-13 00:08:55 -0400

    Changed Tutorials page.

--- js/main.js
+++ js/main.js
@@ -67,6 +67,35 @@ $(document).ready(function(){
 		}
 	});
 
+	// Tutorials Accordion Toggling
+
+		$('.tutorials-accordion h4').click(function(){
+		if ($(window).width() <= 749){
+			if ($(this).hasClass('open-tab')) {
+				$('.tutorials-accordion div').slideUp();
+				$('.tutorials-accordion h4').removeClass('open-tab');
+			} else {
+				$('.tutorials-accordion div').slideUp();
+				$('.tutorials-accordion h4').removeClass('open-tab');
+				$(this).next('div').slideToggle();
+				$(this).toggleClass('open-tab');
+			}
+		}
+	});
+	
+
+	var accrd = document.getElementsByClassName("accrdn");
+	var iacrd;
+
+	for (iacrd = 0; iacrd < accrd.length; iacrd++) {
+		accrd[iacrd].onclick = function(){
+			this.classList.toggle("active");
+			this.nextElementSibling.classList.toggle("show");
+	  }
+	}
+		
+
+
     // Desktop Navigation Toggling on Scroll
 
 	if ($(this).scrollTop() > 420 && $(window).width() >= 750){
@@ -157,69 +186,3 @@ $body.scrollspy({
 		});
 	});
 })(jQuery);
-/*
-( function( $ ) {
-$( document ).ready(function() {
-$('#cssmenu li.has-sub>a').on('click', function(){
-		$(this).removeAttr('href');
-		var element = $(this).parent('li');
-		if (element.hasClass('open')) {
-			element.removeClass('open');
-			element.find('li').removeClass('open');
-			element.find('ul').slideUp();
-		}
-		else {
-			element.addClass('open');
-			element.children('ul').slideDown();
-			element.siblings('li').children('ul').slideUp();
-			element.siblings('li').removeClass('open');
-			element.siblings('li').find('li').removeClass('open');
-			element.siblings('li').find('ul').slideUp();
-		}
-	});
-
-	$('#cssmenu>ul>li.has-sub>a').append('<span class="holder"></span>');
-
-	(function getColor() {
-		var r, g, b;
-		var textColor = $('#cssmenu').css('color');
-		textColor = textColor.slice(4);
-		r = textColor.slice(0, textColor.indexOf(','));
-		textColor = textColor.slice(textColor.indexOf(' ') + 1);
-		g = textColor.slice(0, textColor.indexOf(','));
-		textColor = textColor.slice(textColor.indexOf(' ') + 1);
-		b = textColor.slice(0, textColor.indexOf(')'));
-		var l = rgbToHsl(r, g, b);
-		if (l > 0.7) {
-			$('#cssmenu>ul>li>a').css('text-shadow', '0 1px 1px rgba(0, 0, 0, .35)');
-			$('#cssmenu>ul>li>a>span').css('border-color', 'rgba(0, 0, 0, .35)');
-		}
-		else
-		{
-			$('#cssmenu>ul>li>a').css('text-shadow', '0 1px 0 rgba(255, 255, 255, .35)');
-			$('#cssmenu>ul>li>a>span').css('border-color', 'rgba(255, 255, 255, .35)');
-		}
-	})();
-
-	function rgbToHsl(r, g, b) {
-	    r /= 255, g /= 255, b /= 255;
-	    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-	    var h, s, l = (max + min) / 2;
-
-	    if(max == min){
-	        h = s = 0;
-	    }
-	    else {
-	        var d = max - min;
-	        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-	        switch(max){
-	            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-	            case g: h = (b - r) / d + 2; break;
-	            case b: h = (r - g) / d + 4; break;
-	        }
-	        h /= 6;
-	    }
-	    return l;
-	}
-});
-} )( jQuery ); */
